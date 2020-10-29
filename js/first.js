ada1ceab7f86b5ff690e1708581a799578c41b09
hasankarim18 <hallysust@gmail.com>
2020-10-29 20:42:40 +0600

    p-edumodu-1

---
+++ js/first.js
@@ -0,0 +1,39 @@
+
+
+//$('.counter').counterUp();
+
+
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+
+// stick up started code goes here
+
+//initiating jQuery
+jQuery(function ($) {
+    $(document).ready(function () {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('.navbar-wrapper').stickUp();
+    });
+});
+
+
+//mixit up starter code goes here
+
+
+
+var mixer = mixitup('.university');
+$(function () {
+    $('.Container').mixItUp();
+});
+
+
+
+
+// pi chart intiation code
+
